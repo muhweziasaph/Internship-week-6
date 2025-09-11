439ad172e75233a6033562f8bebafb732328e5d2
MUHWEZI ASAPH <muhweziasaph33@gmail.com>
2025-09-11 04:02:27 +0300

    Update User.js

--- my_to_do_app_final_project - Copy/backend/models/User.js
+++ my_to_do_app_final_project - Copy/backend/models/User.js
@@ -1,13 +1,32 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    password: { type: String }, // hashed (or temp first-time token hashed)
-    isFirstLogin: { type: Boolean, default: true },
-    resetToken: { type: String, default: null }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isFirstLogin: {
+      type: Boolean,
+      default: true, // true when using temp token, false after setting real password
+    },
+    resetToken: {
+      type: String,
+      default: null, // for forgot-password flow
+    },
   },
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
